e9488dcb6dc7db76fd5bda7389825a70d0bda845
Parrot <parrot@localhost.localdomain>
2022-03-10 03:43:30 +0300

    fix error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+echo "This is a tests" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
